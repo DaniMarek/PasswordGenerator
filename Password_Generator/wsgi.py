d6dbec1489c5661b1c5b28315ebcd48c1bfcdd32
Dani Marek <32878738+DaniMarek@users.noreply.github.com>
2019-05-02 18:34:18 -0500

    third deploy to heroku

--- Password_Generator/wsgi.py
+++ Password_Generator/wsgi.py
@@ -9,10 +9,6 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 
 import os
 
-from whitenoise.django import DjangoWhiteNoise
-
-application = DjangoWhiteNoise(application)
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Password_Generator.settings")
